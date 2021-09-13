14f40cc8d3d5e9c2e8e5b4cf8cdbbd32f42ea6c1
Sergey <biznesgroup2017@gmail.com>
2021-09-13 18:15:33 +0300

    4-d home work

--- app.js
+++ app.js
@@ -1,35 +1,61 @@
 
 
+function concatStrings(str, separator){
+  separator = "";
+  let res = "";  
+    const newFunc = function(input){
+      if(!input === separator) return res;
+      if(typeof input !== "string") return res;   
+      res += input;
+      return newFunc;
+    }
+      
+    return newFunc(str)
+}
 
-Array.prototype.myFilter = function(callback, thisAgr) {   
 
-    return this.reduce(function(accum, elem, index, array){
-        if(callback(thisAgr, elem, index, array )){
-            accum.push(elem);
-            
-        }
-        return accum;
-    },[])
+
+
+
+class Calculator {
+
+  constructor(one, two ){
+      
+    if(one === "") throw new Error("Ошибка")
+    if(typeof one !== "number" || typeof two !== "number" || arguments.length > 2) throw new Error("Ошибка")
+      this.one = one;
+      this.two = two;
+      this.logDiv = this.logDiv.bind(this)
+      this.logMul = this.logMul.bind(this)
+      
   }
 
-  const myFilter = arr.myFilter((elem) => elem > 5)
+     setX(val){
+      if(typeof val !== "number" || val === undefined || isNaN(val)) throw new Error("Ошибка")
+        this.one = val;
+        return val;
+    
+  }
+  setY(val2){
+    if(typeof val2 !== "number" || val2 === undefined || isNaN(val2)) throw new Error("Ошибка!")
+    this.two = val2;
+    return val2;
+  }
 
+  logSum(){
+    console.log(this.one + this.two);
+  }
+  logMul(){
+    console.log(this.one * this.two);
+  }
+  logSub(){
+    console.log(this.one - this.two);
+  }
+  logDiv = () => {
+    if(this.val2 === 0) throw new Error("Ошибка!")
+    console.log(this.one / this.two);
+  }
 
-
-
-  function createDebouncedFunction(callback, delay) {
-
-    let timerId = null;
-
-    return function () {
-        if (timerId) {
-            clearTimeout(timerId);
-            timerId = null;
-        }
-
-        timerId = setTimeout(function () {
-            callback();
-            timerId = null;
-        }, delay);
-    }
 }
+
+
